08bbea8ae8b1f5c3caa0cfc1e578bf7398c3e58d
John Mark Redding <johnmarkredding@gmail.com>
2018-12-11 14:49:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class Application < Sinatra::Base
-  # Write your code here!
-
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/greet' do
+    erb :greet
+  end
+  
+  post '/greet' do
+    erb :greet
+  end
 end
